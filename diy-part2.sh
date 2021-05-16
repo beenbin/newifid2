7d5fd52599eec4f209964d50b72f76a4e7f63bb4
Bingo <61998928+beenbin@users.noreply.github.com>
2021-05-16 20:20:30 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -15,3 +15,5 @@ sed -i 's/192.168.1.1/192.168.3.3/g' package/base-files/files/bin/config_generat
 git clone https://github.com/beenbin/Lienol_Passwall.git package/passwall
 #git clone https://github.com/tty228/luci-app-serverchan.git package/luci-app-serverchan
 git clone https://github.com/jefferymvp/luci-app-koolproxyR.git package/luci-app-koolproxyR
+git clone https://github.com/rufengsuixing/luci-app-adguardhome.git package/luci-app-adguardhome
+git clone -b master https://github.com/vernesong/OpenClash.git package/Openclash
